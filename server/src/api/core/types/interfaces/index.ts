0a563a3f9af0161662898965825ffee221344b70
RajendraDex <rajendrap.dexbytes@gmail.com>
2025-05-23 14:14:15 +0530

    # added app boostrap and  database config files

---
+++ server/src/api/core/types/interfaces/index.ts
@@ -0,0 +1,15 @@
+import { ICache } from './cache.interface';
+import { IHTTPError } from './http-error.interface';
+import { IRequest } from './request.interface';
+import { IStorage } from './storage.interface';
+import { IUploadOptions } from './upload-options.interface';
+
+
+export {
+	ICache,
+	IHTTPError,
+	IRequest,
+	IStorage,
+	IUploadOptions,
+
+}
